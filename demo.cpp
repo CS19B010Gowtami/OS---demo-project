fcd89955a5e475471b243ec900137cc2460f0f9d
GithinGeorge <githin2k1@pop-os.localdomain>
2022-02-12 15:29:19 +0530

    File added

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h> 
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+}
