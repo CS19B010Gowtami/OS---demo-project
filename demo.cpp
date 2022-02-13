9ab4189963732be24f635eb32ea1250615ac2f02
Githin George <70936358+GithinGeorge2k1@users.noreply.github.com>
2022-02-13 21:04:34 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -12,79 +12,87 @@ int main(int argc,char *argv[])
     char *readStream = new char[1];
     int bytes_read=0;
     
-    int readFD[3];
-
-    readFD[0] =  open("./Files/X.txt",O_RDONLY);
-    readFD[1] =  open("./Files/Y.txt",O_RDONLY);
-    readFD[2] =  open("./Files/Z.txt",O_RDONLY);
-
-        // 3 File Des -> Rd Permissions
-        // Code Size Can be reduced with a loop
-    for(int i=0;i<3;i++)
-    {
-       if(readFD[i]>-1)
-       {
-           bytes_read = read(readFD[i],readStream,sizeof(char));
-           if(bytes_read)
-              cout<<readStream<< " ";
-       }
-       else
-        cout<<"_ ";
-    }
-    cout << endl << "-----------------------------------------------"<<endl;
-
-    close(readFD[0]);
-    close(readFD[1]);
-    close(readFD[2]);
-
-    //  Special User with admin Rights..!
-
-    int writeFD[3];
-    char *writeStream=new char[1];
-
-
-    writeFD[0] =  open("./Files/X.txt",O_WRONLY);
-    writeFD[1] =  open("./Files/Y.txt",O_WRONLY);
-    writeFD[2] =  open("./Files/Z.txt",O_WRONLY);
-
-    bool access= (writeFD[0]>-1 || writeFD[1]>-1 || writeFD[2]>-1);
-    
-    // 3 File Des -> Rd Permissions
-    // 3 File Des -> Write Mode-> 
-    
     bool inProgram = true;
-    while(inProgram && access)
-    {
+    
+    while(inProgram){
+        
+        int readFD[3];
+
+        readFD[0] =  open("./Files/X.txt",O_RDONLY);
+        readFD[1] =  open("./Files/Y.txt",O_RDONLY);
+        readFD[2] =  open("./Files/Z.txt",O_RDONLY);
+
+            // 3 File Des -> Rd Permissions
+            // Code Size Can be reduced with a loop
+        cout<<"The Values in Files X Y and Z are respectively: "<<endl;
         for(int i=0;i<3;i++)
         {
-            if(writeFD[i]>-1)
-                cout<< "Enter "<<i+1<< "to Change Value of file "<<i+1<<endl;
+           if(readFD[i]>-1)
+           {
+               bytes_read = read(readFD[i],readStream,sizeof(char));
+               if(bytes_read)
+                  cout<<readStream<< " ";
+           }
+           else
+            cout<<"_ ";
         }
-        cout<<"Enter 4 to Exit The Program"<<endl;
-        cout << endl;
-        // Show Options For editing  
-        int option;
-        cin>> option; 
-        cin.ignore();  
-        if(option==1 || option==2 || option==3)
+        cout << endl << "-----------------------------------------------"<<endl;
+
+        close(readFD[0]);
+        close(readFD[1]);
+        close(readFD[2]);
+
+        //  Special User with admin Rights..!
+
+        int writeFD[3];
+        char *writeStream=new char[1];
+
+
+        writeFD[0] =  open("./Files/X.txt",O_WRONLY);
+        writeFD[1] =  open("./Files/Y.txt",O_WRONLY);
+        writeFD[2] =  open("./Files/Z.txt",O_WRONLY);
+
+        bool access= (writeFD[0]>-1 || writeFD[1]>-1 || writeFD[2]>-1);
+
+        // 3 File Des -> Rd Permissions
+        // 3 File Des -> Write Mode-> 
+
+        if(access)
         {
-            cout<<"Enter Value To Replace in the file: ";
-            cin>>writeStream;  
-            cout<<writeStream<<endl;
-            lseek(writeFD[option-1],0,SEEK_SET);
-            write(writeFD[option-1],writeStream,sizeof(char));
+            for(int i=0;i<3;i++)
+            {
+                if(writeFD[i]>-1)
+                    cout<< "Enter "<<i+1<< "to Change Value of file "<<i+1<<endl;
+            }
+            cout<<"Enter 4 to Exit The Program"<<endl;
+            cout << endl;
+            // Show Options For editing  
+            int option;
+            cin>> option; 
+            cin.ignore();  
+            if(option==1 || option==2 || option==3)
+            {
+                cout<<"Enter Value To Replace in the file: ";
+                cin>>writeStream;  
+                cout<<writeStream<<endl;
+                lseek(writeFD[option-1],0,SEEK_SET);
+                write(writeFD[option-1],writeStream,sizeof(char));
+            }
+            else if(option==4)
+            {
+                inProgram = false;
+            }
+            else
+            {
+                cout<<"Please Enter A Valid Option"<<endl;
+            }
+            cout<<endl;
         }
-        else if(option==4)
-        {
-            inProgram = false;
-        }
-        else
-        {
-            cout<<"Please Enter A Valid Option"<<endl;
-        }
-        cout<<endl;
+        close(writeFD[0]);
+        close(writeFD[1]);
+        close(writeFD[2]);
+        
+        cout << endl << "-----------------------------------------------"<<endl;
+        
     }
-    close(writeFD[0]);
-    close(writeFD[1]);
-    close(writeFD[2]);
 }
